f2ca0b7cf2651cf0a1c8b5e6671bb3ee0c6c8970
Mushahid Khisal Ansari <mushahid.study@gmail.com>
2025-07-02 20:33:14 +0530

    solving build error

--- nextApp/lib/interface.ts
+++ nextApp/lib/interface.ts
@@ -1,13 +1,27 @@
-export interface simpleBlogCard{
-    title: string
-    smallDescription: string
-    currentSlug: string
-    titleImage: any
+import { PortableTextBlock } from '@portabletext/types';
+
+export interface simpleBlogCard {
+  title: string;
+  smallDescription: string;
+  currentSlug: string;
+  titleImage: {
+    _type: 'image';
+    asset: {
+      _ref: string;
+      _type: 'reference';
+    };
+  };
 }
 
 export interface Fullblog {
-    currentSlug: string
-    title: string
-    Content: any
-    titleImage: any
+  currentSlug: string;
+  title: string;
+  Content: PortableTextBlock[];
+  titleImage: {
+    _type: 'image';
+    asset: {
+      _ref: string;
+      _type: 'reference';
+    };
+  };
 }
--- nextApp/lib/sanity.ts
+++ nextApp/lib/sanity.ts
@@ -1,5 +1,6 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from '@sanity/asset-utils';
 
 export const client = createClient({
   projectId: "0ok4io1e",
@@ -9,6 +10,6 @@ export const client = createClient({
 });
 
 const builder = imageUrlBuilder(client)
-export function urlFor(source: any){
-  return builder.image(source)
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source);
 }
